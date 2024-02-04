aff162057ebb5e9645720e50fa484a9235a09360
TarunPereddi <tarun.pereddi.work@gmail.com>
2024-02-04 12:46:57 +0530

    validation check

--- index.js
+++ index.js
@@ -2,27 +2,34 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 
-const API_KEY = process.env.WEATHER_API_KEY;;
+const API_KEY = process.env.WEATHER_API_KEY;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 app.post('/getWeather', async (req, res) => {
   const cities = req.body.cities;
+
+  if (!cities || cities.length === 0) {
+    return res.status(400).json({ message: 'Invalid input: No cities provided' });
+  }
+
   const weatherData = {};
 
   for (let city of cities) {
-    const response = await axios.get(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=${API_KEY}&contentType=json`);
-    const weather = response.data.currentConditions;
-    weatherData[city] = weather.temp + 'C';
+    try {
+      const response = await axios.get(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=${API_KEY}&contentType=json`);
+      const weather = response.data.currentConditions;
+      weatherData[city] = weather.temp + 'C';
+    } catch (error) {
+      return res.status(400).json({ message: `Invalid place: ${city}` });
+    }
   }
 
   res.json({ weather: weatherData });
-  console.log({ weather: weatherData })
 });
 
-app.listen(3001, ()=>{
-  console.log("Server Started")
+app.listen(3001, () => {
+  console.log("Server Started");
 });
